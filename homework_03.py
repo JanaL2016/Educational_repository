84b8b4e577a185ea4245a019240eb201abc3406b
JanaL2016 <100992728+JanaL2016@users.noreply.github.com>
2024-07-16 14:02:28 +0300

    Add files via upload

---
+++ homework_03.py
@@ -0,0 +1,5 @@
+Number_OfHomework_Assignments = 12
+Number_Of_hours_Spent = 1.5
+Course_Name = "Python"
+Average_Time_Per_Task = Number_Of_hours_Spent/Number_OfHomework_Assignments
+print("Курс: " + Course_Name + "," + " всего задач:" + str(Number_OfHomework_Assignments) + "," + " затрачено часов:", str(Number_Of_hours_Spent) + ",", "среднее время выполнения", str(Average_Time_Per_Task) + " часа.")
