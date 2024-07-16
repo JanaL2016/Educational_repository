cee98fabddf214e37a36bf5e74e2bfdc4b74d31b
JanaL2016 <100992728+JanaL2016@users.noreply.github.com>
2024-07-16 23:40:55 +0300

    Add files via upload

---
+++ homework_05.py
@@ -0,0 +1,5 @@
+name = "Евгений"
+age = 26
+age = age + 1
+is_student = True
+print("Имя: " + name + "\nВозраст: " + str(age - 1) + "\nНовый возраст: " + str(age) + "\nЯвляеться ли студентом: " + str(is_student))
